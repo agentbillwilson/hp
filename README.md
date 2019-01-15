a47248e0daf903d0818df6674058015d801c9c38
agentbillwilson <36679974+agentbillwilson@users.noreply.github.com>
2019-01-15 02:33:12 -0500

    Add Hiscores lookup

--- README.md
+++ README.md
@@ -11,12 +11,12 @@ Building
 Usage
 -----
 
-    hp -h xp \
-    [-a xp] [-s xp] [-d xp] [-r xp] \
+    hp [-n name] \
+    [-h xp] [-a xp] [-s xp] [-d xp] [-r xp] \
     [-A level] [-S level] [-D level] [-R level]
 
 hp estimates the Hitpoints level achieved from obtaining other combat levels in
-Old School RuneScape. -h, -a, -s, -d, and -r specify the Hitpoints, Attack,
-Strength, Defence, and Ranged experience points to begin with, respectively.
--A, -S, -D, and -R specify the target Attack, Strength, Defence, and Ranged
-levels, respectively.
+Old School RuneScape. -n specifies a player name to look up in the Hiscores.
+-h, -a, -s, -d, and -r specify the Hitpoints, Attack, Strength, Defence, and
+Ranged experience points to begin with, respectively. -A, -S, -D, and -R
+specify the target Attack, Strength, Defence, and Ranged levels, respectively.
--- hp.go
+++ hp.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"strconv"
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"log"
 	"math"
+	"net/http"
+	"net/url"
 	"os"
+)
+
+type Skill int
+
+const (
+	Attack Skill = iota + 1
+	Defence
+	Strength
+	Hitpoints
+	Ranged
 )
 
 // XP returns the experience points necessary to achieve level.
@@ -16,17 +31,18 @@ func XP(lvl int) int {
 	return sum / 4
 }
 
-const usage = `usage: hp -h xp \
-[-a xp] [-s xp] [-d xp] [-r xp] \
+const usage = `usage: hp [-n name] \
+[-h xp] [-a xp] [-s xp] [-d xp] [-r xp] \
 [-A level] [-S level] [-D level] [-R level]
 
 hp estimates the Hitpoints level achieved from obtaining other combat levels in
-Old School RuneScape. -h, -a, -s, -d, and -r specify the Hitpoints, Attack,
-Strength, Defence, and Ranged experience points to begin with, respectively.
--A, -S, -D, and -R specify the target Attack, Strength, Defence, and Ranged
-levels, respectively.`
+Old School RuneScape. -n specifies a player name to look up in the Hiscores.
+-h, -a, -s, -d, and -r specify the Hitpoints, Attack, Strength, Defence, and
+Ranged experience points to begin with, respectively. -A, -S, -D, and -R
+specify the target Attack, Strength, Defence, and Ranged levels, respectively.`
 
 func main() {
+	name := flag.String("n", "", "player name")
 	hp := flag.Int("h", 0, "Hitpoints XP")
 	atk := flag.Int("a", 0, "Attack XP")
 	str := flag.Int("s", 0, "Strength XP")
@@ -40,24 +56,67 @@ func main() {
 		fmt.Fprintln(os.Stderr, usage)
 	}
 	flag.Parse()
-	if *hp == 0 {
+	if *name == "" && *hp == 0 {
 		flag.Usage()
 		os.Exit(1)
 	}
-	xpTotal := 0
-	if xp := XP(*atkLvl) - *atk; xp > 0 {
-		xpTotal += xp
+	xp := map[Skill]int{
+		Attack:    *atk,
+		Strength:  *str,
+		Defence:   *def,
+		Hitpoints: *hp,
+		Ranged:    *rng,
 	}
-	if xp := XP(*strLvl) - *str; xp > 0 {
-		xpTotal += xp
+	if *name != "" {
+		fmt.Println(*name)
+		u, err := url.Parse("https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws")
+		if err != nil {
+			log.Fatal(err)
+		}
+		v := url.Values{}
+		v.Set("player", *name)
+		u.RawQuery = v.Encode()
+		fmt.Println(u.String())
+		res, err := http.Get(u.String())
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
+		r := csv.NewReader(res.Body)
+		if _, err := r.Read(); err != nil {
+			log.Fatal(err)
+		}
+		for skill := Attack; skill <= Ranged; skill++ {
+			record, err := r.Read()
+			if err != nil {
+				log.Fatal(err)
+			}
+			xp[skill], err = strconv.Atoi(record[2])
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
-	if xp := XP(*defLvl) - *def; xp > 0 {
-		xpTotal += xp
+	levels := map[Skill]int{
+		Attack:    *atkLvl,
+		Defence:   *defLvl,
+		Strength:  *strLvl,
+		Ranged:    *rngLvl,
 	}
-	if xp := XP(*rngLvl) - *rng; xp > 0 {
-		xpTotal += xp
+	newXP := 0
+	for skill := Attack; skill <= Ranged; skill++ {
+		if skill == Hitpoints {
+			continue
+		}
+		lvl := levels[skill]
+		if lvl == 0 {
+			continue
+		}
+		if xp := XP(lvl) - xp[skill]; xp > 0 {
+			newXP += xp
+		}
 	}
-	*hp += xpTotal * 3 / 4
+	*hp = xp[Hitpoints] + (newXP * 3 / 4)
 	hpLvl := 0
 	for lvl := 10; XP(lvl) <= *hp; lvl++ {
 		hpLvl = lvl
